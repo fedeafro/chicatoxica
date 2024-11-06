f31a9cbe762be071bddcaba62f0315c2c529d72f
fedeafro <86858316+fedeafro@users.noreply.github.com>
2024-11-06 16:24:05 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+
+window.sr = ScrollReveal();
+
+  sr.reveal('.nombre-banda', {
+      duration: 2000,
+      origin: 'top',
+      distance: '-100px'
+  });
+
+  sr.reveal('.wrapper', {
+      duration: 4000,
+      origin: 'right',
+      distance: '-100px'
+  });
+
+  sr.reveal('.videoxxx', {
+    duration: 1000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.videofx', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
